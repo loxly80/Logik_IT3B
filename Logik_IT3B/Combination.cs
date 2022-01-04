8fb943f5aed97b1d3b14ea483e401c2e1f83d145
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-01-04 13:46:16 +0100

    Add project files.

---
+++ Logik_IT3B/Combination.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Logik_IT3B
+{
+    public class Combination
+    {
+        public Pin[] Pins { get; set; }
+
+        public Combination()
+        {
+            Pins = new Pin[] {
+                new Pin(),
+                new Pin(),
+                new Pin(),
+                new Pin()
+            };
+        }
+
+        public void Draw(Graphics graphics, PointF location, int size)
+        {
+            for (int i = 0; i < Pins.Length; i++)
+            {
+                Pins[i].Draw(graphics, Pens.Black, new PointF(location.X + i * size * 1.25F, location.Y), size);
+            }
+        }
+    }
+}
---
+++ Logik_IT3B/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Logik_IT3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBox = new System.Windows.Forms.PictureBox();
+            this.btnEvaluate = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBox
+            // 
+            this.pBox.BackColor = System.Drawing.Color.White;
+            this.pBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pBox.Location = new System.Drawing.Point(12, 12);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(378, 447);
+            this.pBox.TabIndex = 0;
+            this.pBox.TabStop = false;
+            // 
+            // btnEvaluate
+            // 
+            this.btnEvaluate.Location = new System.Drawing.Point(12, 465);
+            this.btnEvaluate.Name = "btnEvaluate";
+            this.btnEvaluate.Size = new System.Drawing.Size(158, 34);
+            this.btnEvaluate.TabIndex = 1;
+            this.btnEvaluate.Text = "Vyhodnotit";
+            this.btnEvaluate.UseVisualStyleBackColor = true;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(231, 465);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(158, 34);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "Nová hra";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 511);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnEvaluate);
+            this.Controls.Add(this.pBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Logik";
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBox;
+        private System.Windows.Forms.Button btnEvaluate;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
